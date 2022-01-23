e359e58663b4d1a91eba4729972cc73222496797
syedalirazazaidi <46336938+syedalirazazaidi@users.noreply.github.com>
2022-01-23 10:20:15 +0500

    add basic

---
+++ bin/exercises01.dart
@@ -0,0 +1,16 @@
+void main(List<String> arguments) {
+  // int a = 5;
+  // int b = a--;
+  // int c = ++b;
+  // int d = b += 2;
+
+  // print(a);
+  // print(b);
+  // print(c);
+  // print(d);
+  // print(b);
+  const value = 9;
+  if (value % 3 == 0) {
+    print('heello');
+  }
+}
